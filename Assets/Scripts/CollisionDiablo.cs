a8d14a101b9721bd30074af1b806fe8e56614ba1
m-luiisa <44697160+m-luiisa@users.noreply.github.com>
2018-11-28 11:38:10 +0100

    test

---
+++ Assets/Scripts/CollisionDiablo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CollisionDiablo : MonoBehaviour
+{
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.tag == "Player")
+    {
+      Debug.Log("Stell dich doch nicht so blöd an!");
+      SceneManager.LoadScene("GreenLevel1");
+    }
+  }
+}
