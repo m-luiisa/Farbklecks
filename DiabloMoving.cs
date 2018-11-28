af82d00741e94bc481318f81e87eea6bc37484e0
m-luiisa <44697160+m-luiisa@users.noreply.github.com>
2018-11-28 08:51:09 +0100

    Add files via upload

---
+++ DiabloMoving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiabloMoving : MonoBehaviour
+{
+  private Vector3 startPos;
+  private Vector3 newPos;
+  public float speed =1f;
+  // Use this for initialization
+  void Start ()
+  {
+    startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    newPos = startPos;
+    newPos.x += Mathf.PingPong(Time.time * speed, 0.6F);
+    transform.position = newPos;
+  }
+}
---
+++ neustart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class neustart : MonoBehaviour {
+
+  // Use this for initialization
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag== "Player")
+    {
+      Debug.Log("Stell dich doch nicht so blöd an!");
+      SceneManager.LoadScene("GreenLevel1");
+    }
+  }
+}
