04275389bd8b1ca52a6045513c70716d621b0d8d
m-luiisa <44697160+m-luiisa@users.noreply.github.com>
2018-12-05 11:49:11 +0100

    Update Shooting
    in allen Level schießen
    
    alle getroffenen Gegner färben sich grün

--- Assets/Scripts/CollisionDiablo.cs
+++ Assets/Scripts/CollisionDiablo.cs
@@ -6,20 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class CollisionDiablo : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other)
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.tag == "Player")
     {
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("Stell dich doch nicht so blöd an!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-
-        if (other.gameObject.tag == "Green")
-        {
-            Debug.Log("GRün hat getroffen!");
-            GameObject.Find("DiabloKörper").GetComponent<Renderer>().material.color = Color.green;
-            Destroy(other.gameObject);
-        }
-
+      Debug.Log("Stell dich doch nicht so blöd an!");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    if (other.gameObject.tag == "Green")
+    {
+      Debug.Log("Grün hat getroffen!");
+      GetComponent<Renderer>().material.color = Color.green;
+      Destroy(other.gameObject);
+    }
+
+  }
 }
