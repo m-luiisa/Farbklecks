f849b68099e544659a804852c66f6d90a59ca5d0
Marc Kloos <marc.kloos@outlook.de>
2019-01-02 23:01:48 +0100

    ENDLICH: Boden verfärbt sich :D
    Aaaalso:
    
    Wenn man mit der grünen Farbe den Boden trifft kommt ein Farbklecks dahin
    
    Sollte man dann darauf laufen stribt der Spieler
    
    Yeah :D
    
    PRO: Die anderen Farben sind jetzt super einfach erweiterbar und das mit dem Effekt wenn man auf dem Bereich läuft ist ziemlich gut erweiterbar

---
+++ Assets/Scripts/BulletGreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGreen : MonoBehaviour {
+
+  public Transform spawn;
+  public GameObject clone;
+  public GameObject greenGround;
+
+ 
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.tag == "Green")
+    {
+      Destroy(other.gameObject);
+      Debug.Log("Grün hat auf Boden getroffen!");
+      //GetComponent<Renderer>().material.color = Color.green;
+      spawn = this.transform;
+      clone = Instantiate(greenGround, spawn.position, spawn.rotation);
+    }
+
+  }
+}
---
+++ Assets/Scripts/GreenGround.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GreenGround : MonoBehaviour {
+
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.tag == "Player")
+    {
+      Debug.Log("Stell dich doch nicht so blöd an!");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+  }
+}
