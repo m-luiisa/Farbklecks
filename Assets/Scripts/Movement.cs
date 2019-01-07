26b6b24a3edc6a2a8bc807e92f3f932c249008b9
Marc Kloos <marc.kloos@outlook.de>
2019-01-07 22:55:14 +0100

    Leben und Gegner-Movement
    Marc versucht wieder alles hinzubekommen

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+  Rigidbody2D myBody;
+  Transform myTrans;
+  public float speed;
+  public float length;
+  float start;
+  float end;
+
+
+
+  // Use this for initialization
+  void Start()
+  {
+    myTrans = this.transform;
+    myBody = this.GetComponent<Rigidbody2D>();
+    start = myTrans.position.x;
+    end = myTrans.position.x - length;
+
+  }
+
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    //Debug.Log(myTrans.position.x);
+    //Debug.Log(end);
+    
+    if (myTrans.position.x < end || myTrans.position.x > start)
+    {
+      Vector3 currRot = myTrans.eulerAngles;
+      currRot.y += 180;
+      myTrans.eulerAngles = currRot;
+      //Debug.Log("CHANGED");
+    }
+
+    Vector2 myVel = myBody.velocity;
+    myVel.x = -myTrans.right.x * speed;
+    myBody.velocity = myVel;
+
+  }
+}
