7adc3a572ce3e1d76b4c32cc6450c58d1b4a48de
juelide <44697077+juelide@users.noreply.github.com>
2019-01-09 22:47:38 +0100

    Tutorial funktioniert

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+  public void ClickCyan()
+  {
+    SceneManager.LoadScene("Cyan1");
+  }
+
+  public void ClickAgain()
+  {
+    SceneManager.LoadScene("Cyan1");
+  }
+}
+
--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -45,7 +45,7 @@ namespace UnityStandardAssets._2D
       m_Anim.SetBool("isGrounded", m_Grounded);
 
       // Set the vertical animation
-      m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
+      //m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
       if (m_Rigidbody2D.velocity.y > 0)
         m_Anim.SetBool("isGrounded", false);
       else
